44079f635f989163fa70c84a0b692f32bebda762
Aung Myat Kyaw <aungmyatkyaw@Aungs-MacBook-Pro.local>
2023-03-26 12:39:54 +0630

    add random password generating function

--- index.js
+++ index.js
@@ -13,4 +13,18 @@ $(document).ready(function(){
         show();
         setTimeout(hide,800);
     });
+
+
+$('.generate-btn').click(function(ß){
+    var length = 12,
+    charSet = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|:"<>?-=[];',./~`,
+    randPass = '';
+
+    for (var i =0; i < length; i++) {
+        var randNum = Math.floor(Math.random()*charSet.length);
+        randPass += charSet.substring(randNum, randNum+1);
+    }
+    $('#text').val(randPass);
+});
+
 });
